0303deaf80258bed34863fdb15d087d18f1fd15a
Luka Latkovic <latkovic.luka@gmail.com>
2020-06-13 02:04:06 +0200

    Added section-orders dummy data and template

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
+
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +10,12 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[]=[
+    {id: 1, customer:
+      {id: 1, name: 'Main st Bakery', state: 'SO', email: 'test@test.com'},
+       total: 230, placed: new Date(2020, 12, 1), fullfiled: new Date(2020, 12, 1)},
+  ];
 
   ngOnInit() {
   }
